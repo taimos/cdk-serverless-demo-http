d425fd16e06dd9615378a5407a2279284a00fdcc
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2021-05-19 18:01:22 +0200

    changes

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { HttpApiAspect } = require('cdk-serverless/lib/projen');
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.98.0',
+  cdkVersion: '1.100.0',
   cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
@@ -10,6 +10,11 @@ const project = new AwsCdkTypeScriptApp({
   deps: [
     'cdk-serverless',
   ],
+  tsconfig: {
+    compilerOptions: {
+      allowSyntheticDefaultImports: true,
+    },
+  },
 });
 
 new HttpApiAspect(project, {
